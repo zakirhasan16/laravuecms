672b922598ee649cc1ff38b3d4c6d409172356a4
Zakir Hasan <zakirhasan16@icloud.com>
2020-09-02 14:34:34 +0300

    - Laravel Spatie-MediaLibrary Integration.

--- app/Blog.php
+++ app/Blog.php
@@ -3,9 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Blog extends Model
+class Blog extends Model implements HasMedia
 {
+    use InteractsWithMedia;
+
     protected $fillable = [
         'title',
         'url',
@@ -15,5 +19,10 @@ class Blog extends Model
     public function category()
     {
         return $this->belongsTo(BlogCategory::class, 'category_id','id');
+    }
+
+    public function getImageAttribute()
+    {
+        return $this->getMedia('image')->first();
     }
 }
